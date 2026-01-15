25dca4a304d721676a6ba161d47220f3c1e6a428
Jeongtae Shin <shinjeongtae@gmail.com>
2026-01-16 02:12:55 +0900

    [ADD] dconf

---
+++ run_once_before_10-apply-dconf-settings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Apply cursor size and text scaling factor
+gsettings set org.gnome.desktop.interface cursor-size 40
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.33
